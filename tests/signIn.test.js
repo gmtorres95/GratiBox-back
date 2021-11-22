8acb6cd59cf0dcd2f00de7263d5b302c7d664058
Gabriel Torres <al2012.163.torres@gmail.com>
2021-11-22 16:15:52 -0300

    fix: config connectionString

--- tests/signIn.test.js
+++ tests/signIn.test.js
@@ -10,7 +10,7 @@ describe('POST /sign-in', () => {
   let userId;
 
   beforeAll(async () => {
-    userId = insertUser(userData);
+    userId = await insertUser(userData);
   });
 
   afterAll(async () => {
